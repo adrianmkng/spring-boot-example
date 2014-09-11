73beff2539fe3cc3fe9ab9d0faa4336d15c2b198
Adrian Ng <n9.adrian@gmail.com>
2014-09-11 21:57:48 +0100

    Fixing import sql so that users references account.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,12 +1,21 @@
 CREATE DATABASE IF NOT EXISTS `spring_boot_example` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `spring_boot_example`;
 
+-- structure for table account
+CREATE TABLE IF NOT EXISTS `account` (
+  `email` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- structure for table spring_boot_example.users
-CREATE TABLE IF NOT EXISTS `users`` (
+CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `enabled` boolean NOT NULL DEFAULT '1',
-  PRIMARY KEY (`username`)
+  UNIQUE index username_idx_1 (`username`),
+  FOREIGN KEY (`username`) REFERENCES account(`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- structure for table spring_boot_example.user_roles
@@ -15,13 +24,4 @@ CREATE TABLE IF NOT EXISTS `authorities` (
   `authority` varchar(50) NOT NULL,
   FOREIGN KEY (`username`) REFERENCES users (`username`),
   UNIQUE index authorities_idx_1 (`username`, `authority`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- structure for table spring_boot_example.account
-CREATE TABLE IF NOT EXISTS `account` (
-  `email` varchar(255) NOT NULL,
-  `firstname` varchar(255) NOT NULL,
-  `lastname` varchar(255) NOT NULL,
-  FOREIGN KEY (`email`) REFERENCES users (`username`)
-  UNIQUE index email_idx_1 (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
